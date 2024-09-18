b8a16b8f6ab4823befbfbf3acd0d8134ea79d261
EC97B0EAB79C <19406885+EC97B0EAB79C@users.noreply.github.com>
2024-09-18 16:57:56 +0900

    Added keyword extraction base code

--- paper_rel_gen.py
+++ paper_rel_gen.py
@@ -32,7 +32,7 @@ from openai import OpenAI
 endpoint = "https://models.inference.ai.azure.com"
 token = os.environ["GITHUB_TOKEN"]
 
-# OpenAI Embedding
+OpenAI Embedding
 from azure.ai.inference import EmbeddingsClient
 from azure.core.credentials import AzureKeyCredential
 
@@ -42,11 +42,39 @@ client = EmbeddingsClient(
     credential=AzureKeyCredential(token)
 )
 
-response = client.embed(
+embedding_response = client.embed(
     input=[markdown],
     model=embedding_model_name
 )
 
 
 # OpenAI Keyword Extraction
+from pydantic import BaseModel
+
+chat_model_name = "gpt-4o-mini"
+client = OpenAI(
+    base_url=endpoint,
+    api_key=token,
+)
+
+GPT_INSTRUCTIONS = """
+This GPT helps users generate a set of relevant keywords or tags based on the content of any note or text they provide. It offers concise, descriptive, and relevant tags that help organize and retrieve similar notes or resources later. The GPT will aim to provide up to 8 keywords, with 3 of them being general tags applicable to a broad context, and 5 being more specific to the content of the note. It avoids suggesting overly generic or redundant keywords unless necessary. It will list the tags using underscores instead of spaces, ordered from the most general to the most specific. Every tag will be lowercase.
+Return the list in json format with key "keywords".
+"""
+
+messages = [
+    {"role":"system", "content": GPT_INSTRUCTIONS},
+    {"role": "user", "content": markdown},
+]
+
+completion = client.beta.chat.completions.parse(
+    model = chat_model_name,
+    messages = messages,
+    response_format = { "type": "json_object" }
+)
+
+chat_response = completion.choices[0].message
+
+import json
+keywords = json.loads(chat_response.content)["keywords"]
 
