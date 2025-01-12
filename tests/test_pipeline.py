ab75723ef799d1ee0590cca931d63e293811e3e3
EC97B0EAB79C <19406885+EC97B0EAB79C@users.noreply.github.com>
2025-01-12 16:47:13 +0900

    Test pytest

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,4 +1,17 @@
 import pytest
+for src.paper_rel_gen import *
 
 def test_test():
     assert 1==1
+
+
+def test_extract_yaml():
+    test_data=[
+        "---",
+        "test_entry: 'The quick brown fox jumps over the lazy dog'"
+        "---"
+    ]
+
+    result = extract_yaml(test_data)
+
+    assert result == {"test_entry": "The quick brown fox jumps over the lazy dog"}
