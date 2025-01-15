6bc3b1514a08133a1aaae66ddabf22bdc5396a3f
EC97B0EAB79C <19406885+EC97B0EAB79C@users.noreply.github.com>
2025-01-15 16:37:17 +0900

    Updated Test Cases

---
+++ tests/test_paper_rel_gen.py
@@ -0,0 +1,21 @@
+import pytest
+
+from src.paper_rel_gen import *
+
+class TestSBKeyGeneration:
+    @pytest.mark.parametrize("title, author, year, expected", [
+        (
+            "The Test Title", 
+            "Test, Author", 
+            "2099", 
+            "test..2099the...ttt............."
+        ),
+        (
+            "LongerFirst a a a a a a a a a a a a a a a a a a a a", 
+            "LongTest, Author", 
+            2099, 
+            "longte2099longerlaaaaaaaaaaaaaaa"
+        )
+    ])
+    def test_key_generation(self, title, author, year, expected):
+        assert generate_sbkey(title, author, year) == expected
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,9 @@
 import pytest
-from src.paper_rel_gen import *
 
-class TestDataExtraction:
+from src.utils.md import MarkdownUtils
+from src.utils.text import TextUtils
+
+class TestMarkdownUtils:
     @pytest.fixture
     def yaml_test_data(self):
         return [
@@ -17,7 +19,7 @@ class TestDataExtraction:
         return """
 ```BibTeX
 @article{test2099article,
-  title={Article Name for Testing BiBTex Extraction},
+  title={Article Name for Testing BibTex Extraction},
   author={Test, Author1 and Testing, Author2 and Another, Test3},
   journal={Test Case Journal},
   year={2099}
@@ -26,7 +28,7 @@ class TestDataExtraction:
 """
 
     def test_extract_yaml(self, yaml_test_data):
-        metadata, body = extract_yaml(yaml_test_data)
+        metadata, body = MarkdownUtils.extract_yaml(yaml_test_data)
 
         assert metadata["test_entry"] == "The quick brown fox jumps over the lazy dog"
         assert len(body) != 0
@@ -34,30 +36,25 @@ class TestDataExtraction:
     def test_extract_bibtex(self, bibtex_test_data):
 
 
-        result = extract_bibtex(bibtex_test_data)
+        result = MarkdownUtils.extract_bibtex(bibtex_test_data)
         expected_result = {
                     "title": "Article Name for Testing BibTex Extraction",
                     "author": ["Test, Author1", "Testing, Author2", "Another, Test3"],
-                    "year": "2099",
-                    "journal": "Test Case Journal"
+                    "year": 2099,
+                    "bibtex_key": "test2099article"
                 }
         assert result == expected_result
 
-
-class TestSBKeyGeneration:
-    @pytest.mark.parametrize("title, author, year, expected", [
-        (
-            "The Test Title", 
-            "Test, Author", 
-            "2099", 
-            "test..2099the...ttt............."
-        ),
-        (
-            "LongerFirst a a a a a a a a a a a a a a a a a a a a", 
-            "LongTest, Author", 
-            2099, 
-            "longte2099longerlaaaaaaaaaaaaaaa"
-        )
+class TestTextUtils:
+    @pytest.mark.parametrize("input_text, expected", [
+        # Basic cases
+        ("Hello, World!", "hello world"),
+        ("a!b@c#1$2%3", "a b c 1 2 3"),
+        ("test-case_example", "test case example"),
     ])
-    def test_key_generation(self, title, author, year, expected):
-        assert generate_sbkey(title, author, year) == expected
+    def test_clean(self, input_text, expected):
+        assert TextUtils.clean(input_text) == expected
+
+    def test_same(self):
+        assert TextUtils.same("Test, text!", "test text")
+        assert not TextUtils.same("Test, text!", "another test text")
