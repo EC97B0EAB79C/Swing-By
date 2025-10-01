644ddc583805b85037eb5144c734ad599ded4901
EC97B0EAB79C <19406885+EC97B0EAB79C@users.noreply.github.com>
2025-10-01 17:39:54 +0900

    Settings Interface

--- settings.ts
+++ settings.ts
@@ -2,17 +2,63 @@ import SwingBy from './main';
 
 import { App, PluginSettingTab, Setting } from 'obsidian';
 
+const modelOptions = [
+    { name: 'Gemini 2.5 Flash', value: 'gemini/gemini-2.5-flash' },
+    { name: 'Gemini 2.5 Flash-Lite Preview', value: 'gemini/gemini-2.5-flash-lite-preview-06-17' },
+    { name: 'Gemini 2.0 Flash', value: 'gemini/gemini-2.0-flash' },
+    { name: 'Gemini 2.0 Flash-Lite', value: 'gemini/gemini-2.0-flash-lite' },
+    { name: 'Gemini 1.5 Flash', value: 'gemini/gemini-1.5-flash' },
+    { name: 'Gemini 1.5 Pro', value: 'gemini/gemini-1.5-pro' },
+    { name: 'OpenAI GPT-4.1-nano', value: 'openai/gpt-4.1-nano' },
+    { name: 'OpenAI GPT-4.1-mini', value: 'openai/gpt-4.1-mini' },
+    { name: 'OpenAI GPT-4.1', value: 'openai/gpt-4.1' },
+    { name: 'OpenAI GPT-4o', value: 'openai/gpt-4o' },
+    { name: 'OpenAI GPT-4o mini', value: 'openai/gpt-4o-mini' },
+    { name: 'OpenAi o4-mini', value: 'openai/o4-mini' },
+    { name: 'OpenAi o3-mini', value: 'openai/o3-mini' },
+    { name: 'OpenAi o3', value: 'openai/o3' },
+    { name: 'OpenAI o1', value: 'openai/o1' },
+    { name: 'Sonar Pro', value: 'pplx/sonar-pro' },
+    { name: 'Sonar', value: 'pplx/sonar' },
+];
+
+// Article settings interface
 export interface ArticleSettings {
     apiKeyADS: string;
 }
-
+// LLM settings interface
+export interface LlmApiKeys {
+    openai?: string;
+    gemini?: string;
+}
+export interface Endpoint {
+    openai: string;
+}
+export interface LlmSettings {
+    selectedModel: string;
+    apikeys: LlmApiKeys;
+    endpoint: Endpoint;
+}
+// settings interface
 export interface SwingBySettings {
     article: ArticleSettings;
+    llm: LlmSettings;
 }
+
 
 export const DEFAULT_SETTINGS: SwingBySettings = {
     article: {
         apiKeyADS: '',
+    },
+    llm: {
+        selectedModel: 'gemini/gemini-2.5-flash',
+        apikeys: {
+            openai: '',
+            gemini: '',
+        },
+        endpoint: {
+            openai: '',
+        },
     },
 };
 
